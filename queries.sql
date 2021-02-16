9c457f118c32ce787645f38150086a386ffa941e
Will Walles <willwalles@Wills-MacBook-Pro.local>
2021-02-15 21:32:11 -0600

    Finished code for all querry questions

--- queries.sql
+++ queries.sql
@@ -24,3 +24,50 @@ select dm.dept_num, d.dept_name, dm.emp_num, e.last_name, e.first_name
 		on dm.dept_num=d.dept_num
 	left join employees as e
 		on dm.emp_num=e.emp_num;
+		
+--4.List the department of each employee with the following information:
+----employee number, last name, first name, and department name.
+select * from employees;
+select * from employee_dept;
+select * from departments;
+select e.emp_num, e.last_name, e.first_name, d.dept_name, ed.dept_num 
+from employees as e
+left join employee_dept as ed
+	on e.emp_num=ed.emp_num
+left join departments as d
+	on ed.dept_num=d.dept_num;
+	
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select * from employees;
+SELECT e.first_name, e.last_name, e.sex
+from employees as e
+WHERE first_name = 'Hercules' AND last_name like 'B%';
+
+-- 6. List all employees in the Sales department, 
+---including their employee number, last name, first name, and department name.
+
+select e.emp_num, e.last_name, e.first_name, d.dept_name,ed.dept_num
+from employees as e
+left join employee_dept as ed
+	on e.emp_num=ed.emp_num
+left join departments as d
+	on ed.dept_num=d.dept_num
+	where d.dept_name = 'Sales';
+	
+-- 7.List all employees in the Sales and Development departments
+---, including their employee number, last name, first name, and department name.
+
+select e.emp_num, e.last_name, e.first_name, d.dept_name,ed.dept_num
+from employees as e
+left join employee_dept as ed
+	on e.emp_num=ed.emp_num
+left join departments as d
+	on ed.dept_num=d.dept_num
+	where d.dept_name = 'Sales' or d.dept_name = 'Development';
+
+-- 8. In descending order, list the frequency count of employee 
+---last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(*) AS freq_count
+FROM employees
+GROUP BY last_name
+ORDER BY freq_count DESC;
